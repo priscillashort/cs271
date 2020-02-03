ab32750e5759b5e5e14f85087973bb23b3766ef4
priscillashort <pshort2@cocc.edu>
2020-02-03 15:51:57 -0800

    First Pass of mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//@multiplicand
+//M=2
+//@multiplier
+//M=3
+
+
+@i
+M=1
+@sum
+M=0
+(LOOP)
+    @i
+    D=M
+    @R0
+    D=D-M
+    @END
+    D;JGT
+    @R1
+    D=M
+    @sum
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+@sum
+D=M
+@R2
+M=D
+@END
+0;JMP
