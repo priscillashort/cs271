619c681dd46185b153c40ffd2e22cb2f166e8283
priscillashort <pshort2@cocc.edu>
2020-03-09 13:33:53 -0700

    First draft of C parser

--- 06/assembler/c_parser.rb
+++ 06/assembler/c_parser.rb
@@ -2,11 +2,62 @@ require_relative 'variable_manager'
 
 class CParser
 
-	attr_accessor :line, :variable_manager
+	attr_accessor :line, :variable_manager, :jump_bits, :ac_bits, :dest_bits
 
   def initialize(line, variable_manager)
 		@line = line
 		@variable_manager = variable_manager
+		@dest_bits = {
+			"" => "000",
+			"M" => "001",
+			"D" => "010",
+			"MD" => "011",
+			"A" => "100",
+			"AM" => "101",
+			"AD" => "110",
+			"AMD" => "111"
+		}
+		@ac_bits = {
+			"" => "0000000",
+			"0" => "0101010",
+			"1" => "0111111",
+			"-1" => "0111010",
+			"D" => "0001100",
+			"A" => "0110000",
+			"!D" => "0001101",
+			"!A" => "0110011",
+			"-D" => "0001111",
+			"-A" => "0110011",
+			"D+1" => "0011111",
+			"A+1" => "0110111",
+			"D-1" => "0001110",
+			"A-1" => "0110010",
+			"D+A" => "0000010",
+			"D-A" => "0010011",
+			"A-D" => "0000111",
+			"D&A" => "0000000",
+			"D|A" => "0010101",
+
+			"M" => "1110000",
+			"!M" => "1110001",
+			"M+1" => "1110111",
+			"M-1" => "1110010",
+			"D+M" => "1000010",
+			"D-M" => "1010011",
+			"M-D" => "1000111",
+			"D&M" => "1000000",
+			"D|M" => "1010101",
+		}
+		@jump_bits = {
+			"" => "000",
+			"JGT" => "001",
+			"JEQ" => "010",
+			"JGE" => "011",
+			"JLT" => "100",
+			"JNE" => "101",
+			"JLE" => "110",
+			"JMP" => "111"
+		}
 	end
 	
 	def parse
@@ -15,7 +66,29 @@ class CParser
 	end
 
 	def parse_c_instruction
+		split_on_eq = line.split('=')
 
+		if split_on_eq.size > 2 || split_on_eq.size == 0
+			raise "Invalid C instruction"
+		end
+
+		before_eq = split_on_eq.size == 1 ? "" : split_on_eq[0]
+		after_eq = split_on_eq.size == 1 ? split_on_eq[0] : split_on_eq[1]
+		split_on_semi = after_eq.split(';')
+
+		if split_on_semi.size > 2 || split_on_semi.size == 0
+			raise "Invalid C instruction"
+		end
+
+		before_semi = split_on_semi[0]
+		after_semi = split_on_semi.size == 1 ? "" : split_on_semi[1]
+
+		result = ""
+		result << ac_bits[before_semi]
+		result << dest_bits[before_eq]
+		result << jump_bits[after_semi]
+
+		puts "111" << result
 	end
 
 end
--- 06/assembler/variable_manager.rb
+++ 06/assembler/variable_manager.rb
@@ -3,7 +3,24 @@ class VariableManager
 	attr_accessor :variables, :variable_counter, :program_line_counter
 
   def initialize
-		@variables = {}
+		@variables = {
+			:R0 => 0,
+			:R1 => 1,
+			:R2 => 2,
+			:R3 => 3,
+			:R4 => 4,
+			:R5 => 5,
+			:R6 => 6,
+			:R7 => 7,
+			:R8 => 8,
+			:R9 => 9,
+			:R10 => 10,
+			:R11 => 11,
+			:R12 => 12,
+			:R13 => 13,
+			:R14 => 14,
+			:R15 => 15,
+		}
 		@variable_counter = 16
 		@program_line_counter = 0
 	end
